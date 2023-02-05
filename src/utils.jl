d18e775ebb3eccc57d9b1dfe5d8a1e9d44cf0ca0
DianaKorladinova <chanchunche@gmail.com>
2023-02-05 16:49:39 +0100

    Added plot functions

--- src/utils.jl
+++ src/utils.jl
@@ -1,10 +1,10 @@
 using NPZ
-export get_data, classification, compute_error
+using Plots
+export get_data, classification, compute_error, create_plot
 
 # Load data
 function get_data()
-    # labels, images = npzread("/home/dianka/Documents/School/JUL/data_33rpz_03_minimax.npz", ["images_test", "labels_test"])
-    labels,images =  npzread("/home/dianka/Documents/School/JUL/data_33rpz_03_minimax.npz", ["images_test", "labels_test"])
+    labels, images = npzread("/home/dianka/Documents/School/JUL/data_33rpz_03_minimax.npz", ["images_test", "labels_test"])
     # We need the second element of the Pair labels
     labels = labels[2]
     # Get the images from the Pair as an array
@@ -33,4 +33,25 @@ function compute_error(lbls::Vector, true_lbls::Vector)
     diff = findall(a->a != 0, d)
     err = size(diff)[1]/size(lbls)[1]
     return err
+end
+
+# Create separation line
+separ3(x::Real, alpha) = (-alpha[3]-alpha[1]*x)/alpha[2]
+separ2(x::Real, alpha) = -alpha[2]-alpha[1]*x
+
+# Create plot
+function create_plot(x::Vector, alpha::Vector, dim_num::Int64, title::String)
+    x1 = x[1:size(x)[1]÷2]
+    x2 = x[size(x)[1]÷2+1:end]
+    p = plot(x1,zeros(15),seriestype=:scatter, color=:pink, title=title)
+    p = plot(p, x2,zeros(15),seriestype=:scatter, color=:purple)
+    xlims = extrema(x) .+ [-0.1, 0.1]
+    if dim_num == 3
+        p = plot(p,xlims, x -> separ3(x,alpha); label = "Separation", line = (:black,3))
+    elseif dim_num == 2
+        p = plot(p,xlims, x -> separ2(x,alpha); label = "Separation", line = (:black,3))
+    else
+        throw(ArgumentError("Invalid dimension"))
+    end
+    return p
 end
