d1a2fb850e7ed8768106dcfeaf70eb7c88cb344c
DianaKorladinova <chanchunche@gmail.com>
2023-02-05 16:52:16 +0100

    Added perceptron option

--- src/kozinec.jl
+++ src/kozinec.jl
@@ -1,11 +1,24 @@
-export kozinec, get_k, perceptron
+export Algorithm, Perceptron, Kozinec
+export kozinec
+
+abstract type Algorithm end
+
+struct Perceptron <: Algorithm end
+struct Kozinec <: Algorithm end
+
+function update(::Kozinec, alpha::Vector, x_t::Vector)
+    k = get_k(alpha, x_t)            
+    alpha = (1-k)*alpha + k*x_t  
+    return alpha
+end
+update(::Perceptron, v::Vector, x_t::Vector) = v + x_t
 
 function get_k(a::Vector, x::Vector)
     res = -sum(a.*(x-a))/sum((x-a).^2)
     return res
 end
 
-function kozinec(X::Matrix, y::Vector; max_iter::Int = 2000)
+function kozinec(X::Matrix, y::Vector; alg::Algorithm = Kozinec(), max_iter::Int = 2000)
     X_my = copy(X')
     iter = copy(max_iter)
     # Get indices where yi = 1
@@ -23,8 +36,7 @@ function kozinec(X::Matrix, y::Vector; max_iter::Int = 2000)
         idx = findfirst(a->a <= 0, dots)  
         if typeof(idx) != Nothing
             idx = idx[1]
-            k = get_k(alpha, X_my[idx, :])            
-            alpha = (1-k)*alpha + k*X_my[idx, :]                     
+            alpha = update(alg, alpha,X_my[idx, :])           
         else
             okays += 1
         end        
@@ -36,45 +48,7 @@ function kozinec(X::Matrix, y::Vector; max_iter::Int = 2000)
     end
     if iter == 0
         alpha = Nothing
-    # else
-    #     w = alpha[1:end-1]
-    #     b = alpha[end]
     end
     return alpha
-end
-
-function perceptron(X::Matrix, y::Vector; max_iter::Int = 400)
-    X_my = copy(X')
-    iter = max_iter
-    # Get indices where yi = 1
-    indices = findall(a->a==-1,y)       
-    for i in indices
-        X_my[i, :] *= -1        
-    end
-    v = zeros(size(X_my)[2])       
-    while iter > 0
-        okays = 0
-        dots = sum(v' .* X_my, dims = 2)        
-        # Find the first wrongly classified vector - the dot product is negative
-        idx = findfirst(a->a <= 0, dots)            
-        if typeof(idx) != Nothing
-            idx = idx[1]
-            v += X_my[idx, :]                    
-        else
-            okays += 1
-        end        
-        # If you can't find a faulty one
-        if okays == 1
-            break
-        end
-        iter -= 1
-    end
-    if iter == 0
-        v = Nothing
-    # else
-    #     w = alpha[1:end-1]
-    #     b = alpha[end]
-    end
-    return v
 end
 
